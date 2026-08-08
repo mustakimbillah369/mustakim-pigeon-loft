3ba5e44ceec040721eb6075df93bbb9ed33ead18
mustakimbillah369 <billah.mustakim30401@gmail.com>
2026-08-09 00:07:34 +0600

    add gallery section

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -5,24 +5,22 @@ menuBtn.addEventListener("click", function () {
   mobileMenu.classList.toggle("hidden");
 });
 
-
-
 // JavaScript for Modal Handling
 
-  function openModal(title, content) {
-    document.getElementById('modalTitle').innerText = title;
-    document.getElementById('modalContent').innerText = content;
-    document.getElementById('articleModal').classList.remove('hidden');
-  }
+function openModal(title, content) {
+  document.getElementById("modalTitle").innerText = title;
+  document.getElementById("modalContent").innerText = content;
+  document.getElementById("articleModal").classList.remove("hidden");
+}
 
-  function closeModal() {
-    document.getElementById('articleModal').classList.add('hidden');
-  }
+function closeModal() {
+  document.getElementById("articleModal").classList.add("hidden");
+}
 
-  // Close modal when clicking outside the box
-  window.onclick = function(event) {
-    const modal = document.getElementById('articleModal');
-    if (event.target === modal) {
-      closeModal();
-    }
+// Close modal when clicking outside the box
+window.onclick = function (event) {
+  const modal = document.getElementById("articleModal");
+  if (event.target === modal) {
+    closeModal();
   }
+};
